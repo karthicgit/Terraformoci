45c10070c6bd836049f85bb4d76d81e504879536
KarthicRavindran <121283483+ocigitwork@users.noreply.github.com>
2023-03-24 11:30:24 +0530

    Update variables.tf

--- containerinstance/variables.tf
+++ containerinstance/variables.tf
@@ -44,3 +44,49 @@ variable "createdgandpolicy" {
   type    = bool
   default = false
 }
+
+variable "createvault" {
+  type    = bool
+  default = false
+}
+variable "vault_display_name" {
+  type        = string
+  default     = "CIVault"
+  description = "Vault display name"
+}
+
+variable "vault_type" {
+  type        = string
+  default     = "DEFAULT"
+  description = "Vault type"
+}
+
+variable "freeform_tags" {
+  type = object({
+    vault = map(any),
+    key   = map(any)
+    }
+  )
+  default = {
+    vault = {
+      used_for = "container_instance"
+    }
+    key = {
+      used_for = "container_instance"
+    }
+  }
+  description = "Freeform tags"
+}
+
+
+variable "key_display_name" {
+  type        = string
+  default     = "master"
+  description = "KMS key display names"
+}
+
+variable "key_protection_mode" {
+  type        = string
+  default     = "HSM"
+  description = "Key protection mode SOFTWARE or HSM"
+}
